1b62de146a739c2eafdf4d8c557fb29508e4d334
CM <cm0x0x0x0@gmail.com>
2022-01-11 15:41:26 +0900

    complete ch4

--- Ch04/ArrayRead.c
+++ Ch04/ArrayRead.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void)
-{
-	int arr[10];
-	int readCount = 0;
-	int readData;
-	int i;
+// int main(void)
+// {
+// 	int arr[10];
+// 	int readCount = 0;
+// 	int readData;
+// 	int i;
 
-	while(1)
-	{
-		printf("자연수 입력: ");
-		scanf("%d", &readData);
-		if(readData < 1)
-			break;
+// 	while(1)
+// 	{
+// 		printf("자연수 입력: ");
+// 		scanf("%d", &readData);
+// 		if(readData < 1)
+// 			break;
 
-		arr[readCount++] = readData;
-	}
+// 		arr[readCount++] = readData;
+// 	}
 
-	for(i=0; i<readCount; i++)
-		printf("%d  ", arr[i]);
+// 	for(i=0; i<readCount; i++)
+// 		printf("%d  ", arr[i]);
 
-	return 0;
-}
+// 	return 0;
+// }
